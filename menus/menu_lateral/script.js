59473ce36501c3c6354800563862e1e91663d6f0
Will <willen.garcia05@gmail.com>
2024-05-31 12:24:03 -0300

    terminando projeto menu lateral

--- menus/menu_lateral/script.js
+++ menus/menu_lateral/script.js
@@ -1,8 +1,11 @@
 const botao = document.querySelector('.botao')
 const nav = document.querySelector('.nav')
-const header
+const header = document.querySelector('.cabeca')
+const corpo = document.querySelector('.corpo')
 
 botao.addEventListener('click',()=>{
     nav.classList.toggle('ativo')
+    header.classList.toggle('ativo')
+    corpo.classList.toggle('ativo')
     console.log('ativo')
 })
