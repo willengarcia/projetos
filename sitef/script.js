a2340aad90870022631ad8eb50f3999dd472f4e3
Willen Garcia <89990936+willengarcia@users.noreply.github.com>
2024-07-31 11:50:55 -0300

    verificação

--- sitef/script.js
+++ sitef/script.js
@@ -9,31 +9,25 @@ function pegaCod(cod, endpoint){
     fetch(endpoint)
     .then(res=>res.json())
     .then(res=>{
-        let verificacao = res.filter(obj => obj.cod === codigo)
-        const array = []
-        if(verificacao != array) {
-            alert(`Código ${codigo} inexistente na base de dados!`)
-        }else {
-            res.forEach((objeto)=>{
-                if(objeto.cod == codigo){
-                    h2Codigo.innerHTML = `Código: ${objeto.cod}`
-                    h3Desc.innerHTML = `Descrição: ${objeto.descricao}`
-                    h3Ac.innerHTML = `Ação: ${objeto.acao}`
-                    h3Tentativa.innerHTML = `Retentativa: ${objeto.retentativa}`
-                    cod.value = ''
-                }
-            })
-        }
+        res.forEach((objeto)=>{
+            if(objeto.cod == codigo){
+                h2Codigo.innerHTML = `Código: ${objeto.cod}`
+                h3Desc.innerHTML = `Descrição: ${objeto.descricao}`
+                h3Ac.innerHTML = `Ação: ${objeto.acao}`
+                h3Tentativa.innerHTML = `Retentativa: ${objeto.retentativa}`
+                cod.value = ''
+            }
+        })
     })
     .catch(erro=>{
         alert('Ocorreu um erro ao tentar acessar a base de dados. Erro: '+erro)
     })
 }
 botao.addEventListener('click', (evt)=>{
-    pegaCod(input, 'sitef.json')
+    pegaCod(input, './sitef.json')
 })
 document.addEventListener('keypress',(event)=>{
     if (event.key === 'Enter') {
-        pegaCod(input, 'sitef.json')    
+        pegaCod(input, './sitef.json')    
     }
 });
